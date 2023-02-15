c649a5cab7b8653fb1b45cb61b9ae779a94fb5fe
PierreTembremande <pierre.tembremande@gmail.com>
2023-02-15 16:04:22 +0100

    déplacement dans les enregistrements

--- src/routes/List.js
+++ src/routes/List.js
@@ -3,12 +3,12 @@ import React, { useEffect, useState } from "react";
 export default function lister() {
 
     const [pokes, SetPokes] = useState([]);
-    const [debut, SetDebut] = useState(0);
-    const [fin, SetFin] = useState(20);
+    const [offSet, SetOffSet] = useState(0);
+    const [limite, SetLimite] = useState(0);
     const [motRecherche, SetMotRecherche] = useState("");
     const [newListe, SetNewList] = useState([]);
+
     const update = (value) => {
-        console.log(value)
         SetNewList([])
         for (let i = 0; i < pokes.length; i++) {
             if (pokes[i].name.includes(value)) {
@@ -16,6 +16,18 @@ export default function lister() {
             }
         }
     };
+
+    const pageSuivante = () => {
+        SetOffSet(Number(offSet) + Number(limite));
+    }
+
+    const pagePrecedente = () => {
+        if (offSet - limite >= 0) {
+            SetOffSet(offSet - limite);
+        }
+    }
+
+    console.log(offSet)
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1279').then(res => {
@@ -25,21 +37,37 @@ export default function lister() {
         }).catch(e => {
             console.log(e);
         })
-    }
+    }, []
     );
 
     return (
         <>
             <div>
+                <label>Barre de recherche</label>
                 <input type="text" onChange={({ target: { value } }) => { SetMotRecherche(value); update(value) }}></input>
+                <br /><br />
+                <label>Nombre de pokémons à afficher</label>
+                <input type="number" value={limite} onChange={({ target: { value } }) => { SetLimite(value) }}></input>
             </div>
 
             {
                 <div>
-                    {motRecherche == "" && (
+                    {motRecherche === "" && limite === 0 && (
                         <table>
                             <th>Nom des pokemons</th>
-                            {pokes.map((poke) => (
+                            {pokes.slice(0, pokes.length).map((poke) => (
+                                <tr>
+                                    <td>https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png</td>
+                                    <td>{poke.name}</td>
+                                </tr>
+                            ))}
+                        </table>
+                    )}
+
+                    {limite != 0 && (
+                        <table>
+                            <th>Nom des pokemons</th>
+                            {pokes.slice(offSet, Number(offSet) + Number(limite)).map((poke) => (
                                 <tr>
                                     <td>{poke.name}</td>
                                 </tr>
@@ -61,16 +89,11 @@ export default function lister() {
                 </div>
             }
 
-            {/* <div>
-                <table>
-                    <th>Nom des pokemons</th>
-                    {pokes.slice(0, 20).map((poke) => (
-                        <tr>
-                            <td>{poke.name}</td>
-                        </tr>
-                    ))}
-                </table>
-            </div> */}
+            <div>
+                <button onClick={pagePrecedente}>Précédent</button>
+                <button onClick={pageSuivante}>Suivant</button>
+            </div>
+
 
         </>
     )
